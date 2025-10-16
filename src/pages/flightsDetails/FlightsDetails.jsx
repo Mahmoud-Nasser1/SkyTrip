69b31ee19fa42bf0692ee9688f30e84d23aaa9d9
Ahmed Hussein <ahmedallam5852@gmail.com>
2025-10-17 01:04:18 +0300

    Add Flight-Details page

--- src/pages/flightsDetails/FlightsDetails.jsx
+++ src/pages/flightsDetails/FlightsDetails.jsx
@@ -1,11 +1,22 @@
 import FlightStepper from "./components/FlightStepper";
 import FlightCard from "./components/FlightCard";
+import FlightSummery from "./components/FlightSummery";
+import FlightTaps from "./components/FlightTaps";
 
 const FlightsDetails = () => {
   return (
-    <div className="flex flex-col gap-8">
-      <FlightCard />
-      <FlightStepper />
+    <div className="container mx-auto flex gap-4 justify-center px-6 py-6">
+      <div className="w-3/4 flex flex-col gap-6">
+        <FlightStepper />
+        <FlightSummery />
+        <FlightTaps />
+      </div>
+
+      <div className="w-1/4">
+        <div className="sticky top-6">
+          <FlightCard />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/flightsDetails/components/FlightSummery.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { GiCommercialAirplane } from "react-icons/gi";
+
+import { IoLocationOutline } from "react-icons/io5";
+import { LuShield } from "react-icons/lu";
+import { IoTimeOutline } from "react-icons/io5";
+
+const FlightSummery = () => {
+  return (
+    <div className="flex flex-col shadow-xl p-12 rounded-3xl gap-6">
+      <h1 className="text-xl font-bold">Flight Summary</h1>
+      <div className="grid grid-cols-2 gap-8">
+        <div className="flex items-center gap-4">
+          <section>
+            <GiCommercialAirplane className="text-3xl text-gradient-violet" />
+          </section>
+          <section>
+            <h1 className="text-gray-600">Airline</h1>
+            <h1>SkyWings Airlines</h1>
+          </section>
+        </div>
+        <div className="flex items-center gap-4">
+          <section>
+            <IoLocationOutline className="text-3xl text-gradient-violet" />
+          </section>
+          <section>
+            <h1 className="text-gray-600">Departure</h1>
+            <h1>08:00 AM - New York (JFK)</h1>
+          </section>
+        </div>
+        <div className="flex items-center gap-4">
+          <section>
+            <GiCommercialAirplane className="text-3xl text-gradient-violet" />
+          </section>
+          <section>
+            <h1 className="text-gray-600">Flight Number</h1>
+            <h1>123</h1>
+          </section>
+        </div>
+        <div className="flex items-center gap-4">
+          <section>
+            <IoLocationOutline className="text-3xl text-gradient-violet" />
+          </section>
+          <section>
+            <h1 className="text-gray-600">Arrival</h1>
+            <h1>10:30 PM - Paris (CDG)</h1>
+          </section>
+        </div>
+        <div className="flex items-center gap-4">
+          <section>
+            <IoTimeOutline className="text-3xl text-gradient-violet" />
+          </section>
+          <section>
+            <h1 className="text-gray-600">Duration</h1>
+            <h1>8h 30m</h1>
+          </section>
+        </div>
+        <div className="flex items-center gap-4">
+          <section>
+            <LuShield className="text-3xl text-gradient-violet" />
+          </section>
+          <section>
+            <h1 className="text-gray-600">Class</h1>
+            <h1>Economy</h1>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlightSummery;
