9a2d5bcca2dc0e605e9e19e104d3669d4005d204
Yusuf-bagha <youssefbagha2004@gmail.com>
2025-10-18 23:16:15 +0300

    navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import SignUp from "./pages/signUp/SignUp";
 const App = () => {
   return (
     <div>
-      <Header />
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
@@ -30,7 +30,7 @@ const App = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
