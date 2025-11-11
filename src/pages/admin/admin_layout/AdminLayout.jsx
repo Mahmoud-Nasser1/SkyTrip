3fc24c62e41eab73401db53a0732796e45db0d01
Yusuf-bagha <youssefbagha2004@gmail.com>
2025-11-11 19:00:11 +0200

    created whole admin pages except payments

--- src/pages/admin/admin_layout/AdminLayout.jsx
+++ src/pages/admin/admin_layout/AdminLayout.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../../../components/Sidebar/sidebar'
 export default function adminLayout() {
   return (
     <div className="flex min-h-screen bg-slate-900 text-white">
-      <Sidebar/>
+    <Sidebar/>
       <main className="flex-1 p-6 overflow-y-auto dark:bg-[#10161E]">
         <Outlet />
       </main>
