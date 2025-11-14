feb1965539c5a246fe72c9875ad0d2f1072947d3
Mahmoud_Nasser <mahmoudnasser80m@gmail.com>
2025-11-14 12:28:35 +0200

    UpdateFeaterdDestinationaad

--- src/pages/FeaturedDestinationsDetails/components/CardBestTime.jsx
+++ src/pages/FeaturedDestinationsDetails/components/CardBestTime.jsx
@@ -1,6 +1,6 @@
 const CardBestTime = ({ season, months, weather, icon }) => {
   return (
-    <div className="flex flex-col items-center gap-4 p-5 bg-white duration-500 rounded-2xl shadow-md hover:shadow-lg transition-shadow w-64 dark:bg-dark-card hover:scale-[1.01] hover:border-gradient-violet hover:shadow-gradient-violet/30">
+    <div className="flex flex-col items-center gap-4 p-5 bg-white duration-500 rounded-2xl shadow-md hover:shadow-lg transition-all w-64 dark:bg-dark-card hover:scale-[1.01] hover:border-gradient-violet hover:shadow-gradient-violet/30">
       <div className="w-12 h-12 flex items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500 rounded-xl">
         <span className="text-2xl text-white">{icon}</span>
       </div>
--- src/pages/FeaturedDestinationsDetails/components/TopAttractionsCard.jsx
+++ src/pages/FeaturedDestinationsDetails/components/TopAttractionsCard.jsx
@@ -1,6 +1,6 @@
 const TopAttractionsCard = ({ image, name, description, entryPrice }) => {
   return (
-    <div className="group cursor-pointer rounded-3xl overflow-hidden shadow-xl hover:shadow-2xl transition-all transform duration-300 w-full sm:w-[48%] lg:w-[23%] dark:bg-dark-card hover:scale-[1.01] hover:border-gradient-violet hover:shadow-gradient-violet/30">
+    <div className="group cursor-pointer rounded-3xl overflow-hidden shadow-xl hover:shadow-2xl transition-all duration-500 w-full sm:w-[48%] lg:w-[23%] dark:bg-dark-card hover:scale-[1.02] hover:border-gradient-violet hover:shadow-gradient-violet/30">
       <div className="relative h-48 md:h-52 lg:h-60">
         <img src={image} alt={name} className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
